7cc95150cb439d684689f77fce5c8c91cb7fc76d
Md.ashikujjaman <ashik191022@gmail.com>
2025-10-30 21:21:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -31,11 +31,12 @@ app.use(helmet());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(cors());
 
- app.use(cors({
-  origin: ['http://localhost:5173', 'https://natours-3da0.onrender.com']
+//  app.use(cors({
+//   origin: ['http://localhost:5173', 'https://natours-3da0.onrender.com']
   
-}));
+// }));
 
 // Limit requests from same IP
 const limiter = rateLimiter({
