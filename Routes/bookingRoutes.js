1a03ec72495cb3e73e2c9c1404cd1e0fb7d0b3e4
Md.ashikujjaman <ashik191022@gmail.com>
2025-10-30 18:36:50 +0600

    Create bookingRoutes.js

---
+++ Routes/bookingRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const bookingController = require("../controller/bookingController");
+const authController = require("../controller/autuController"); //  protect middleware
+const { protect, restrictTo } = require("../controller/autuController");
+router.use(authController.protect);
+
+// Booking create (tour id take form URL )
+router.post(
+  "/tour/:tourId",
+  authController.protect,
+  bookingController.createBooking
+);
+
+// self booking
+router.get("/my-bookings", bookingController.getMyBookings);
+
+// Admin - can see all booking
+router.get(
+  "/",
+  authController.restrictTo("admin"),
+  bookingController.getAllBookings
+);
+router.delete(
+  "/:id",
+  protect,
+  restrictTo("admin"),
+  bookingController.deleteBookingById
+);
+
+module.exports = router;
